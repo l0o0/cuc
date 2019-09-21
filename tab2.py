9537aecaa9ae53c14aeb680dd1da485d04efd7d7
l0o0 <linxzh1989@gmail.com>
2019-09-21 18:49:07 +0800

    Add tab2 content to store done tasks.

---
+++ tab2.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from PyQt5 import QtWidgets, QtCore, QtGui 
+from task import TaskLine
+
+
+class TAB2(QtWidgets.QWidget):
+    restore_trigger = QtCore.pyqtSignal(TaskLine)
+    def __init__(self, parent=None):
+        super(TAB2, self).__init__(parent)
+        self.config = self.parent().config
+        self.tasklines_done = []
+        self.layout = QtWidgets.QVBoxLayout()
+
+        # add layout for added tasks
+        self.tab2TaskTable = QtWidgets.QTableWidget()
+        self.tab2TaskTable.setStyleSheet("selection-background-color: #f4f6f6")
+        self.tab2TaskTable.verticalHeader().setVisible(False)
+        self.tab2TaskTable.horizontalHeader().setVisible(False)
+        self.tab2TaskTable.setShowGrid(False)
+        self.tab2TaskTable.setColumnCount(2)
+        self.tab2TaskTable.setRowCount(0)
+        self.tab2TaskTable.setColumnWidth(0, 415)
+        self.tab2TaskTable.setColumnWidth(1, 20)
+        self.tab2TaskTable.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Fixed)
+        self.layout.addWidget(self.tab2TaskTable)
+
+        # signal and slot
+        # update tray icon by taskline number 
+        self.restore_trigger.connect(self.parent().updateTab1Table)
+
+        # display tasks
+        self.layout.setContentsMargins(3,3,3,0)
+        self.setLayout(self.layout)
+
+
+    # create button in taskline
+    def createButton(self):
+        self.butt = QtWidgets.QPushButton()
+        self.butt.setMaximumSize(25, 25)
+        self.butt.setIcon(QtGui.QIcon("icons/restore.png"))      
+        self.butt.clicked.connect(self.restoreButtonAction)     
+        return self.butt
+
+    
+    def restoreButtonAction(self):
+        button = self.sender()
+        if button:
+            row = self.tab2TaskTable.indexAt(button.pos()).row()
+            self.tab2TaskTable.removeRow(row)
+            taskline = self.tasklines_done[row]
+            taskline.mask = None
+            taskline.completion_date = None
+            self.restore_trigger.emit(taskline)
+            del self.tasklines_done[row]
+        print('restore', row, taskline.format_text())
+        # send signal to update tray icon
+
+    
+    def saveDoneTask(self):
+        done_texts = [t.format_text() + '\n' for t in self.tasklines_done]
+        with open(self.config['donetxt'], 'a', encoding='utf-8') as handle:
+            handle.writelines(done_texts)
+            print('save done.txt, %s' % len(self.tasklines_done))
+        
+  
